4fd83e8c432285f1016c142583c2cac3ed1980ca
v.v.demidova <v.v.demidova@nordigy.ru>
2022-04-05 15:43:30 +0300

    add .jar

---
+++ build/resources/main/db/migration/V4__changepassword_db.sql
@@ -0,0 +1,7 @@
+update users
+set password='$2a$12$vdcWELDxPoNH.Gb6V5PteehKAK1pv2oOCmI4l67SszfXZEUs.HYPu'
+where username like 'test';
+
+update users
+set password='$2a$12$9tLBPehGYY5kRRwzoZPQSOsPHUUga.StHilBeldL3oPLQ29eYLA2a'
+where username like 'vika';
